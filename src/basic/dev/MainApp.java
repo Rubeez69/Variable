af07ea2cbdce67c71bc74e6144887c6aa9504661
dang diep <ddiep@gmail.com>
2022-08-23 18:53:57 +0700

    first commit

---
+++ src/basic/dev/MainApp.java
@@ -0,0 +1,21 @@
+package basic.dev;
+
+public class MainApp {
+	public static void main(String[] args) {
+		/*
+		 * Khai báo biến*/
+		String firstName  = "Phan Van";
+		String lastName = "Nam";
+		int old = 15;
+		float markMath = 9f;
+		boolean passed = true;
+		
+		System.out.println("Ho va ten: ");
+		System.out.println(firstName + " " + lastName);
+		System.out.println("Math mark: " + markMath);
+		System.out.println("Passed? " + passed);
+		
+		//Khai báo hằng. Hằng phải viết hoa
+		final float PI = 3.14f;
+	}
+}
